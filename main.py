32aa56bbe8e8c4711ba43e5e33a1e5a21ae3a71c
Just-A-Damar <112915300+Just-A-Damar@users.noreply.github.com>
2022-09-06 09:49:01 +0700

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 def on_forever():
-    basic.show_leds("""
-        . # . . .
-                . # . . .
-                . . . . .
-                . . . . .
-                . # . . .
-    """)
+    if pins.analog_read_pin(AnalogPin.P4) >= 400:
+        music.play_melody("G D F - B D E G ", 300)
+    if pins.analog_read_pin(AnalogPin.P4) <= 400:
+        pass
 basic.forever(on_forever)
+
+def on_forever2():
+    if pins.digital_read_pin(DigitalPin.P0) == 1:
+        basic.show_string("Gempa")
+        music.play_melody("A B F G E C5 A C ", 300)
+        basic.pause(2000)
+    pins.digital_write_pin(DigitalPin.P1, 0)
+    basic.show_number(0)
+basic.forever(on_forever2)
+
+def on_forever3():
+    serial.write_value("Level", pins.analog_read_pin(AnalogPin.P2))
+    basic.show_number(pins.analog_read_pin(AnalogPin.P2))
+basic.forever(on_forever3)
--- main.ts
+++ main.ts
@@ -1,8 +1,24 @@
+// Asep / MQ
 basic.forever(function () {
-    basic.showNumber(pins.analogReadPin(AnalogPin.P4))
     if (pins.analogReadPin(AnalogPin.P4) >= 400) {
-        pins.digitalWritePin(DigitalPin.P5, 1)
-    } else {
-        pins.digitalWritePin(DigitalPin.P5, 0)
+        music.playMelody("G D F - B D E G ", 300)
     }
+    if (pins.analogReadPin(AnalogPin.P4) <= 400) {
+    	
+    }
+})
+// Gempa
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P0) == 1) {
+        basic.showString("Gempa")
+        music.playMelody("A B F G E C5 A C ", 300)
+        basic.pause(2000)
+    }
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    basic.showNumber(0)
+})
+// Air
+basic.forever(function () {
+    serial.writeValue("Level", pins.analogReadPin(AnalogPin.P2))
+    basic.showNumber(pins.analogReadPin(AnalogPin.P2))
 })
